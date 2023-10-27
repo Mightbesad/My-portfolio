475b9a31af2f904fbc1f0c20d295fd18df46de56
Mightbesad <francemma20@gmail.com>
2023-10-27 12:24:32 +0100

    changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -18,22 +18,22 @@ export default {
           imageUrl: 'https://cdn-icons-png.flaticon.com/512/732/732190.png',
           status: 'Advanced'
         },
-
+        
         {
           id: 3,
           name: 'Javascript',
           imageUrl: 'https://cdn.icon-icons.com/icons2/2415/PNG/512/javascript_original_logo_icon_146455.png',
           status: 'Advance'
         },
-
-
+        
+       
         {
           id: 4,
           name: 'VueJS',
           imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-vue-282497.png?f=webp',
           status: 'Intermediate'
         },
-
+        
         {
           id: 5,
           name: 'NodeJS',
@@ -112,55 +112,47 @@ export default {
         class="text-sm md:text-lg text-justify flex flex-col gap-4 md:flex-row md:gap-8 md:justify-left md:items-center">
         <div class="flex justify-center">
           <img class="w-9/12 rounded-full mb-3 fadein-up"
-            src=https://ucarecdn.com/ded01a2d-5a2e-4426-a042-9d4784f230a9/-/preview/500x500/-/quality/smart/-/format/auto />
+            src=https://ucarecdn.com/ded01a2d-5a2e-4426-a042-9d4784f230a9/-/preview/500x500/-/quality/smart/-/format/auto/>
         </div>
         <div class="md:w-7/12 custom-paragraph">
           <p class="mb-3 md:mb-7 fadein-left fadeins-1">
-            In my one-year journey as a web developer, I'm Francis Emmanuel, dedicated to crafting responsive,
-            user-friendly websites.
-            Proficient in both front-end and back-end development, I prioritize aesthetics and user experience.
-            Guided by deep user behavior insights, my mission is to create user-centric, visually appealing digital
-            experiences. Committed to innovation, I strive to build websites that seamlessly cater to user needs.
-            My goal is to enhance the web, one beautiful, user-friendly site at a time.
+             In my one-year journey as a web developer, I'm Francis Emmanuel, dedicated to crafting responsive, user-friendly websites. 
+            Proficient in both front-end and back-end development, I prioritize aesthetics and user experience. 
+            Guided by deep user behavior insights, my mission is to create user-centric, visually appealing digital experiences. Committed to innovation, I strive to build websites that seamlessly cater to user needs.
+             My goal is to enhance the web, one beautiful, user-friendly site at a time.
           </p>
           <p class="mb-3 fadein-left fadeins-2">
-
+            
             My goal is to convey your message and identity in the most creative way. If you are interested in hiring me,
-            please contact me at:
+            please contact me at: 
             <!-- make the contact a card and link -->
           </p>
         </div>
 
         <!-- Email Link -->
         <a href="mailto:francemma20@gmail.com">
-          <button
-            class="fadein-bot fade-500 flex items-center py-2 px-4 mx-auto text-sm font-medium rounded-lg border transition duration-300 md:py-2.5 md:px-5 md:mx-0 text-amber-200 border-amber-200 hover:bg-amber-200 hover:bg-opacity-10 bg-transparent focus:outline-none w-fit">
-            <!-- Email Icon (using an SVG) -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill"
-              viewBox="0 0 16 16">
-              <path
-                d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
-            </svg>
-            <span>Email Me</span>
-          </button>
-        </a>
+  <button class="fadein-bot fade-500 flex items-center py-2 px-4 mx-auto text-sm font-medium rounded-lg border transition duration-300 md:py-2.5 md:px-5 md:mx-0 text-amber-200 border-amber-200 hover:bg-amber-200 hover:bg-opacity-10 bg-transparent focus:outline-none w-fit">
+    <!-- Email Icon (using an SVG) -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+</svg>
+    <span>Email Me</span>
+  </button>
+</a>
 
-        <a href="tel:+2349091190644">
+<a href="tel:+2349091190644">
+   
+<button class="fadein-bot fade-500 flex items-center py-2 px-4 mx-auto text-sm font-medium rounded-lg border transition duration-300 md:py-2.5 md:px-5 md:mx-0 text-amber-200 border-amber-200 hover:bg-amber-200 hover:bg-opacity-10 bg-transparent focus:outline-none w-fit">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+</svg>
+    <span>+234-90-9119-0644</span>
+  </button>
 
-          <button
-            class="fadein-bot fade-500 flex items-center py-2 px-4 mx-auto text-sm font-medium rounded-lg border transition duration-300 md:py-2.5 md:px-5 md:mx-0 text-amber-200 border-amber-200 hover:bg-amber-200 hover:bg-opacity-10 bg-transparent focus:outline-none w-fit">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-              class="bi bi-telephone-fill" viewBox="0 0 16 16">
-              <path fill-rule="evenodd"
-                d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
-            </svg>
-            <span>+234-90-9119-0644</span>
-          </button>
-
-        </a>
+</a>
 
 
-
+ 
 
 
       </section>
@@ -182,13 +174,11 @@ export default {
           <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 mb-5">
             <li class="mr-2">
               <button class="inline-block px-4 py-3 rounded-lg hover:text-white"
-                :class="{ 'text-amber-200 bg-amber-200 bg-opacity-10': activeTab === 1 }" @click="activeTab = 1">Tech
-                Stack</button>
+                :class="{ 'text-amber-200 bg-amber-200 bg-opacity-10': activeTab === 1 }" @click="activeTab = 1">Tech Stack</button>
             </li>
             <li class="mr-2">
               <button class="inline-block px-4 py-3 rounded-lg hover:text-white"
-                :class="{ 'text-amber-200 bg-amber-200 bg-opacity-10': activeTab === 2 }"
-                @click="activeTab = 2">Tools</button>
+                :class="{ 'text-amber-200 bg-amber-200 bg-opacity-10': activeTab === 2 }" @click="activeTab = 2">Tools</button>
             </li>
           </ul>
         </div>
@@ -288,9 +278,9 @@ export default {
 }
 
 .pict {
-  box-shadow: 0px 0px 73px -9px rgba(255, 219, 112, 0.44);
-  -webkit-box-shadow: 0px 0px 73px -9px rgba(255, 219, 112, 0.44);
-  -moz-box-shadow: 0px 0px 73px -9px rgba(255, 219, 112, 0.44);
+  box-shadow: 0px 0px 73px -9px rgba(255,219,112,0.44);
+-webkit-box-shadow: 0px 0px 73px -9px rgba(255,219,112,0.44);
+-moz-box-shadow: 0px 0px 73px -9px rgba(255,219,112,0.44);
 }
 
 
@@ -325,7 +315,6 @@ export default {
     opacity: 0;
     transform: translateX(-100%);
   }
-
   100% {
     opacity: 1;
     transform: translateX(0);
@@ -343,7 +332,6 @@ export default {
     opacity: 0;
     transform: translateX(100%);
   }
-
   100% {
     opacity: 1;
     transform: translateX(0);
@@ -370,15 +358,12 @@ export default {
 .fadein-1 {
   animation-delay: 200ms;
 }
-
 .fadein-2 {
   animation-delay: 400ms;
 }
-
 .fadein-3 {
   animation-delay: 600ms;
 }
-
 .fade-500 {
   animation-delay: 500ms;
 }
@@ -389,4 +374,5 @@ export default {
 /* Apply the Montserrat font to your specific paragraph */
 .custom-paragraph {
   font-family: 'Montserrat', sans-serif;
-}</style>
+}
+</style>
